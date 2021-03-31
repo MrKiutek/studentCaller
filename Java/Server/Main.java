018debde04775a79ca0462b396aae9e137eb2ff3
MrKiutek <kiutek@outlook.fr>
2021-03-31 17:56:11 +0200

    Gestion locale de la BDD + librairies

---
+++ Java/Server/Main.java
@@ -0,0 +1,40 @@
+package Java.Server;
+
+import java.awt.BorderLayout;
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Vector;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.Properties;
+
+
+
+
+
+
+public class Main {
+ 
+
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException, IOException {
+
+        Bdd BDD = new Bdd();
+
+	}	
+
+}
