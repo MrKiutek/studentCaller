156da0aed0725e5a17a0458245372ac9ef9dcbdf
MrKiutek <kiutek@outlook.fr>
2021-04-17 15:43:49 +0200

    fix erreur merge

--- Java/Client/Main.java
+++ Java/Client/Main.java
@@ -1,50 +1,13 @@
-package Java.Client;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Scanner;
-
+import javax.swing.*;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 public class Main {
+ 
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
 
-    public static void main(String[] arg) {
-		Scanner sc = new Scanner(System.in);
-		String text;
-		int portEcouteServeur = 10302;
-		BufferedReader lecteurFichier;
-		BufferedReader entree;
-		PrintStream sortie;
-		String ligne;
-		Socket socket;
-		try {
-			socket = new Socket("192.168.3.11", portEcouteServeur);
-			lecteurFichier = new BufferedReader(new FileReader("test.txt"));
-			entree = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			sortie = new PrintStream(socket.getOutputStream());
-			do{
-				System.out.println(entree.readLine());
-				sortie.println(sc.nextLine());
-				text = entree.readLine();
+        UIManager.setLookAndFeel(new NimbusLookAndFeel());
 
-
-			}while(text.contains("nop"));
-			
-		}
-		catch(FileNotFoundException exc) {
-			System.out.println("Fichier introuvable");
-		}
-		catch(UnknownHostException exc) {
-			System.out.println("Destinataire inconnu");
-		}
-		catch(IOException exc) {
-			System.out.println("Probleme d'entree-sortie");
-		}
-	}
-    
+        Flogin Flog = new Flogin();
+        //Fcharge fcharge= new Fcharge();
+    }
 }
