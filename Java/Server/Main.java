eb4ef3571f75bc7c0cdfa5de1d3aaff8878a4ba5
EthaneEce <ethane.ece@gmail.com>
2021-04-18 23:14:22 +0200

    Add All_cours class + gestion de présence dpuis all_cours

--- Java/Server/Main.java
+++ Java/Server/Main.java
@@ -2,29 +2,30 @@ import com.entities.*;
 import com.json_computer.*;
 import com.socket_gestion.*;
 
-
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 
-
-
-
-
 public class Main {
- 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException, IOException {
+    public static void main(String[] args){
 
-		String maClasse = "ING 4 APP SE";
+        String maClasse = "ING 4 APP SE";
         ArrayList<Eleve> listEleve = new ArrayList<Eleve>();
         listEleve.add(new Eleve("KALIFA", "Ethane", "86BFD1F4", maClasse));
         listEleve.add(new Eleve("COUSCOUS", "Saucisse", "CD77475B", maClasse));
         Classe classe = new Classe(maClasse, listEleve);
         ArrayList<Cours> cours = new ArrayList<Cours>();
-        cours.add(new Cours(classe, "P445", "Projet Technologique", new Date(121,3,19,13,0)));
-        
+        All_Cours all_cours = new All_Cours();
+
+        all_cours.add(new Cours(classe, "P445", "Projet Technologique", new Date(121, 3, 18, 23, 0)));
+        System.out.println("all_cours created");
+
+
+        all_cours.set_present("86BFD1F4", "P445");
+        System.out.println("set present");
+
+
+
+
         Json_doer faiseur = new Json_doer();
         faiseur.make(cours);
         System.out.println("Json Written");
@@ -36,10 +37,9 @@ public class Main {
         cours = faiseur.read();
         System.out.println("Json Read");
 
-		Read_rfid_thread thread_read = new Read_rfid_thread();
-		Send_data_thread thread_send = new Send_data_thread();
+        //Read_rfid_thread thread_read = new Read_rfid_thread();
+        //Send_data_thread thread_send = new Send_data_thread();
 
+    }
 
-	}
-
-	}	
+}
--- Java/Server/com/entities/All_Cours.java
+++ Java/Server/com/entities/All_Cours.java
@@ -2,16 +2,55 @@ package com.entities;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
+
+import org.apache.commons.lang.time.DateUtils;
+
 
 public class All_Cours {
-    private HashMap<Date, Cours> list_with_date;
-    private HashMap<String, HashMap<Date, Cours>> list_with_salle;
+    private ArrayList<Cours> list_cours;
     private DateFormat format;
+    
 
     public All_Cours(){
+        this.list_cours = new ArrayList<Cours>();
+        this.format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");    
+    }
 
-        this.format( new SimpleDateFormat("dd/MM/yyyy HH:mm:ss") );    
+    public void add(Cours cours){
+        this.list_cours.add(cours);
+    }
+
+    public Cours find(Date date, String salle){
+        Date cours_begin, cours_end;
+        
+        for(int i=0; i<this.list_cours.size(); i++){
+
+            cours_begin = this.list_cours.get(i).getDate_heure();
+            cours_end = DateUtils.addMinutes(cours_begin, 90);
+
+            if(date.after(cours_begin) && date.before(cours_end) && salle==this.list_cours.get(i).getSalle()){
+                return this.list_cours.get(i);
+            }
+        }
+
+        return null;
+    }
+
+    public void set_present(String rfid, String salle){
+
+        Date now = new Date(System.currentTimeMillis());
+        Cours actual = this.find(now, salle);
+        if (actual != null){
+            actual.setPresent(rfid);
+        } else {
+            System.out.println("Cours not found");
+        }
+        
+    }
+
+    public ArrayList<Cours> getListCours(){
+        return this.list_cours;
     }
 }
